a499faa6e69aa3b4780e699f80a1d8a75d75e8d4
Merkurev Sergei <MerkurevSergei@yandex.ru>
2025-08-01 04:54:08 +0900

    [DPC-5] Translate all Russian text to English

--- src/main/java/ru/standardsolutions/dams/publisher/core/initializer/UnsupportedDatabaseException.java
+++ src/main/java/ru/standardsolutions/dams/publisher/core/initializer/UnsupportedDatabaseException.java
@@ -1,8 +1,8 @@
 package ru.standardsolutions.dams.publisher.core.initializer;
 
 /**
- * Исключение, выбрасываемое когда тип базы данных не поддерживается
- * или не может быть определен по JDBC URL.
+ * Exception thrown when database type is not supported
+ * or cannot be determined from JDBC URL.
  */
 public class UnsupportedDatabaseException extends RuntimeException {
     
